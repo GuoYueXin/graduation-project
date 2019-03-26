eb14e36ce020e06518b931a9e5c651d9c3ccd017
郭月新 <gyx@choicesoft.com.cn>
2019-03-26 22:50:31 +0800

    修改商品详情样式

--- src/pages/home/components/goodsContent.js
+++ src/pages/home/components/goodsContent.js
@@ -1,105 +1,39 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Card, Row, Col } from 'antd';
-import styles from './goodsContent.less';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Link from 'umi/link'
+import { Card, Row, Col } from 'antd'
+import styles from './goodsContent.less'
 
-const { Meta } = Card;
+const { Meta } = Card
 
 class goodsContent extends Component {
   render() {
     return (
       <Row gutter={16} className={styles.wrap}>
-        <Col sm={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta
-              title="Europe Street beat"
-              description="www.instagram.com"
-            />
-          </Card>
-        </Col>
-        <Col sm={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta
-              title="Europe Street beat"
-              description="www.instagram.com"
-            />
-          </Card>
-        </Col>
-        <Col sm={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta
-              title="Europe Street beat"
-              description="www.instagram.com"
-            />
-          </Card>
-        </Col>
-        <Col sm={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta
-              title="Europe Street beat"
-              description="www.instagram.com"
-            />
-          </Card>
-        </Col>
-        <Col sm={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta
-              title="Europe Street beat"
-              description="www.instagram.com"
-            />
-          </Card>
-        </Col>
-        <Col sm={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta
-              title="Europe Street beat"
-              description="www.instagram.com"
-            />
-          </Card>
-        </Col>
-        <Col sm={8} lg={6}>
-          <Card
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-          >
-            <Meta
-              title="Europe Street beat"
-              description="www.instagram.com"
-            />
-          </Card>
-        </Col>
+        <Link to="/register">
+          <Col md={8} lg={6} className={styles.item}>
+            <Card
+              hoverable
+              style={{ width: 220 }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                />
+              }
+            >
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </Col>
+        </Link>
       </Row>
-    );
+    )
   }
 }
 
-goodsContent.propTypes = {
+goodsContent.propTypes = {}
 
-};
-
-export default goodsContent;
+export default goodsContent
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,22 +1,20 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'dva';
-import styles from './index.less';
-import Search from './components/search';
-import GoodsContent from './components/goodsContent';
+import React, { PureComponent } from 'react'
+import { connect } from 'dva'
+import styles from './index.less'
+import Search from './components/search'
+import GoodsContent from './components/goodsContent'
 
 @connect(({ loading, home }) => ({ loading, home }))
 class index extends PureComponent {
   render() {
-    const {
-      msg
-    } = this.props.home;
+    const { msg } = this.props.home
     return (
       <div className={styles.container}>
         <Search />
         <GoodsContent />
       </div>
-    );
+    )
   }
 }
 
-export default index;
+export default index
